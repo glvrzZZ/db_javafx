66f556676c612666e82740553b69cc089cbe6679
Даниил <130974159+glvrzZZ@users.noreply.github.com>
2025-02-27 23:40:57 +0300

    1

---
+++ src/com/javafx/mytest/config/Props.java
@@ -0,0 +1,23 @@
+package com.javafx.mytest.config;
+
+import java.util.Properties;
+
+public class Props {
+    private static Properties properties;
+
+    public static Properties loadProperties() {
+
+        if (properties != null){
+            return properties;
+        }
+
+        Properties tempProperties = new Properties();
+        tempProperties.setProperty("db.url","jdbc:postgresql://localhost:5432/postgres");
+        tempProperties.setProperty("db.user","postgres");
+        tempProperties.setProperty("db.password","test");
+
+        properties = tempProperties;
+
+        return properties;
+    }
+}
